95945c9d478ed72123452db96075dbe4fdfcb233
Dawid Blom <dawidjblom@gmail.com>
2022-03-28 20:42:30 -0300

    Fixed main project structure, and added the TDD integeration.

---
+++ test/main.c
@@ -0,0 +1,17 @@
+/*********************************************************************
+ * Contents: Main file for project tests.
+ *
+ * Author: Dawid Blom.
+ *
+ * Date: 28/03/2022.
+ *
+ *********************************************************************/
+
+#include "CppUTest/CommandLineTestRunner.h"
+
+
+
+int main(int argc, char **argv)
+{
+	return CommandLineTestRunner::RunAllTests(argc, argv);
+}
