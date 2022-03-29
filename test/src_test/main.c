3e08557f81a04537f50b7a48bd7dd79c3f0b029b
Dawid Blom <dawidjblom@gmail.com>
2022-03-29 18:41:13 -0300

    Updated the test dierectory structure and created a new makefile for testin.

---
+++ test/src_test/main.c
@@ -0,0 +1,19 @@
+/*********************************************************
+ * Contents: Start of observer's tests.
+ *
+ * Author: Dawid Blom.
+ *
+ * Date: 28/03/2022.
+ *
+ * NOTE: This file contains the main function that will 
+ * 	 start all the tests created for observer.
+ ********************************************************/
+
+#include "CppUTest/CommandLineTestRunner.h"
+
+
+
+int main(int argc, char **argv)
+{
+	return CommandLineTestRunner::RunAllTests(argc, argv);
+}
---
+++ test/src_test/test.c
@@ -0,0 +1,27 @@
+/********************************************************
+ * Contents: Tests for observer.
+ *
+ * Author: Dawid Blom.
+ *
+ * Date: 29/03/2022.
+ *
+ *
+ * NOTE: This file contains all tests created during the 
+ * 	 engineering of observer.
+ ********************************************************/
+#include "CppUTest/TestHarness.h"
+
+
+
+
+
+TEST_GROUP(First)
+{
+};
+
+
+
+TEST(First, fail_me)
+{
+	FAIL("First test failed.");
+}
