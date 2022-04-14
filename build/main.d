520888e14f5b12cfa94dbfba36f1f2ae20598bca
Dawid Blom <dawidjblom@gmail.com>
2022-04-14 13:22:46 -0300

    Cleaned up the develop dir after the build

--- build/main.d
+++
@@ -1,8 +0,0 @@
-build/main.o: src/main.c src/../header/core_system.h \
- src/../header/amp_system.h src/../header/services.h
-
-src/../header/core_system.h:
-
-src/../header/amp_system.h:
-
-src/../header/services.h:
